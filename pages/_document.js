f059e91a4f95c6419d80afbf53bea5cc3a33c80e
Sushil Behera <sbehera@mpgbis.com>
2022-12-22 16:13:02 +0530

    vebions update

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-            <link href="/favicon.png" rel="icon" />
+        <link href="/favicon.png" rel="icon" />
             <link href="/favicon.png" rel="apple-touch-icon" />
             <link
             href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600;700&family=Inter:wght@400;500;600;700;800&family=Mochiy+Pop+One&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
