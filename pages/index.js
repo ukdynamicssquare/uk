da3ddfda1d88149ddb1eb07f025364ec580de220
Sushil Behera <sbehera@mpgbis.com>
2023-05-19 13:17:13 +0530

    update meta data index page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Microsoft Dynamics 365 Partner UK, Business Central Partner - Dynamics Square</title>
+        <title>Microsoft Dynamics 365 Partner UK, Business Central Partner - Dynamics Square™</title>
         <meta
           name="description"
           content="As a leading Microsoft Dynamics 365 Partner in the UK, our experts help businesses achieve desired results with the implementation of Dynamics 365 apps such as Business Central, NAV, AX, and Power BI. Contact us Today!"
