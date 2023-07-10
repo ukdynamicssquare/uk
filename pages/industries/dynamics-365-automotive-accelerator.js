2272c357a81efae2974e55ec930dd6c8f8f0b286
susil <susilbhr@gmail.com>
2023-07-10 18:18:46 +0530

    update new page

---
+++ pages/industries/dynamics-365-automotive-accelerator.js
@@ -0,0 +1,296 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Form from "../../components/Form";
+const Finance = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Dynamics 365 Automotive Accelerator | Dynamics Square UK </title>
+        <meta
+          name="description"
+          content="Implement Dynamics 365 Automotive Accelerator to supercharge your automotive business while building strong relationships with customers.  "
+        />
+        <link
+          rel="canonical"
+          href="https://www.dynamicssquare.co.uk/industries/dynamics-365-automotive-accelerator/"
+        />
+        <meta
+          property="og:url"
+          content="https://www.dynamicssquare.co.uk/industries/dynamics-365-automotive-accelerator/"
+        />
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+
+     
+
+      <section className="Solution-banner hero-1 hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mobile-1 align-self-center">
+              <h1>Dynamics 365 Automotive Accelerator </h1>
+              <p>
+                Dynamics 365 Automotive Accelerator is an all-in-one business
+                solution to maintain, manage, and streamline an automotive
+                business while building all-around relationships with customers.{" "}
+              </p>
+              <div>
+                <div className="text-center text-lg-start">
+                  <Link href="#exampleModal">
+                    <a
+                      data-bs-toggle="modal"
+                      className="btn-get-started scrollto"
+                    >
+                      <span>Let's Supercharge your Business!</span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <Image
+                src="/img/vend-business-central-integration.png"
+                alt="business-center-new-banne"
+                width={868}
+                height={487}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <header className="section-header">
+                <h2>What is Dynamics 365 Automotive Accelerator? </h2>
+                <p>
+                  As the name suggests, Dynamics 365 Automotive Accelerator
+                  accelerates the business to achieve exponential growth along
+                  with offering a 360-degree approach for the entire business.{" "}
+                </p>
+                <p>
+                  Built on the CDM (common data model), Dynamics 365 for the
+                  automotive industry supports all business operations that
+                  include sales contracts, deals, warranties, service records,
+                  spare part inventory, inspection, test drives,
+                  customer-vehicle relationships, and after-sales management.
+                </p>
+                <p>
+                  It streamlines the core operations of businesses, like OEMs
+                  and dealerships, by leveraging different integrations to
+                  simplify and power up the operations.
+                </p>
+                <p>
+                  You can easily integrate different Microsoft native
+                  applications with it, which include Power BI, Power Apps, and
+                  the Dynamics 365 suite of applications. Also, you can
+                  integrate the B2C integrations to schedule test drives and
+                  service reminders that lead to an enhanced customer
+                  experience.
+                </p>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom001" style={{marginTop:'-80px'}}>
+        <div className="container">
+          <div className="row p-433">
+            <div className="col-lg-6 align-self-center">
+              <div className="side-heading side-heading-system">
+                <h3>What is Microsoft Dynamics 365? </h3>
+              </div>
+              <div className="custom002">
+                <p>
+                   <Link href="/products/microsoft-dynamics-365/"><a>Microsoft Dynamics 365</a></Link> is a suite of intelligent applications
+                  that contain different CRM (customer relationship management)
+                  and ERP (enterprise resource planning) solutions.{" "}
+                </p>
+                <p>
+                  Embedded with the latest technologies, which include AI, BI,
+                  and ML, all solutions in Dynamics 365 offer automation and
+                  powerful functionalities to deal with all kinds of business
+                  operations.
+                </p>
+
+                <p>
+                  Also, Dynamics 365 apps deliver enhanced productivity and
+                  operational efficiency to offer a superior customer experience
+                  using existing resources.{" "}
+                </p>
+                <p>
+                  MS Dynamics 365 contains powerful solutions like <Link href="/products/dynamics-365-business-central/"><a>D365 Business
+                  Central</a></Link>, <Link href="/products/microsoft-dynamics-365-sales/"><a>D365 Sales</a></Link>, <Link href='/products/microsoft-dynamics-365-finance/'><a>Dynamics 365 Finance</a></Link>, <Link href="/products/microsoft-dynamics-365-customer-service/"><a>Customer Service</a></Link>,
+                  D365 Marketing, and more.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-1 align-self-center"></div>
+            <div className="col-lg-5 align-self-center">
+              <div className="busine_service_bottom-left common-side-form">
+                <Form />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom05">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 align-self-center">
+              <div className="busine_service_bottom-left custom012">
+                <Image
+                  src="/img/microosoft-partner.png"
+                  alt="microosoft-partner.png"
+                  width={625} height={651}
+                />
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <div className="side-heading-custom-01">
+                <h2>What is the Need for an Automotive Accelerator for Dynamics 365? </h2>
+              </div>
+              <div className="custom-p-001">
+                <p>An automotive business consists of several complex processes, from managing different vendors to offering the best experience to customers. There are even different processes that need to be done on time using automation for smooth data flow. </p>
+                <p>Here comes the role of Dynamics 365 for the automotive industry: to streamline different processes while utilizing existing resources for a maximized output. Such a system connects the vendor with the business in real time while also catering the best services to customers. </p>
+                <p>Dynamics 365 Automotive Accelerator offers competitive advantages to deal with changing market conditions and fluctuating customer demands. Such an ERP system directly enhances the efficiency of the business and pushes it toward achieving sustainable success.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="call-to-action-center hero-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="call-to-action-content">
+                <h3>Dynamics 365 Automotive Accelerator </h3>
+                <p>Switch your automotive business from the slow to the fast lane! </p>
+                <div className="text-center m-o-t new-btn-nn">
+                  <Link href="#exampleModal">
+                    <a
+                      data-bs-toggle="modal"
+                      className="btn-get-started scrollto"
+                    >
+                      <span>Get it today! </span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10">
+              <header className="commen-rit-info">
+                <h3>Core Advantages of Dynamics 365 Automotive Accelerator </h3>
+                <p>Dynamics 365 for Automotive Accelerator empowers businesses to build bespoke solutions that cater to the demands of different business metrics. It offers different benefits to customers and vendor partners, which are: </p>
+                <ul>
+                  <li>Leveraging the CDN (common data model), the D365 Automotive Accelerator allows businesses to simplify specific operations while strengthening customer relationships. </li>
+                  <li>Integrating Power Platform, like <Link href="/products/microsoft-power-apps/"><a>Power Apps</a></Link>, enables a business to build specific applications to deal with special customer demands. </li>
+                  <li>To get more business insights, you can connect to other D365 solutions, like D365 Marketing and <Link href="/blog/connected-field-service/"><a>Connected Field Service</a></Link>, to increase upsell and cross-sell opportunities based on data. </li>
+                  <li><Link href="/power-bi-implementation/"><a>Integration with Power BI</a></Link> enables a business to analyze and visualize the entire automotive and customer data that includes inventory, branding, services, and more. </li>
+                  <li>You can connect the system with B2C apps to automate recurring and manual tasks that include scheduling test drives, data entry, and fetching documentation, to build intentional conversations with customers. </li>
+                  <li>Microsoft Dynamics 365 for the automotive industry offers a holistic approach to the entire business with data-driven insights while increasing customer lifetime value. </li>
+                </ul>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <section className="upgard-bg">
+        <div className="container">
+
+          <div className="row pad9x" style={{ paddingBottom: "50px" }}>
+            <div className="col-lg-5 align-self-center">
+              <div className="commen-rit-info">
+                <h3>Customer 360 - auto form </h3>
+                <p>The Customer 360 - auto form contains all essential information about the customer and prospects while empowering the sales representative to build strong relationships with the customer. Also, this data helps the representative start intentional conversations with the customer based on stored data and preferences. </p>
+              
+              </div>
+            </div>
+            <div className="col-lg-7 align-self-center">
+              <div className="commen-lef-pics">
+                <img src="/img/n-1.png" alt="pic" />
+              </div>
+            </div>
+          </div>
+          <div
+            className="row pad9x rever-1 align-items-center"
+            style={{ paddingBottom: "50px" }}
+          >
+            <div className="col-lg-7">
+              <div className="commen-lef-pic">
+                <img src="/img/n-2.png" alt="pic" />
+              </div>
+            </div>
+            <div className="col-lg-5 align-self-center">
+              <div className="commen-rit-info">
+                <h3>B2C portal </h3>
+                <p>The B2C portal shows different parameters that lead to customer interactions. It includes car configuration, inventory browsing, forums, community data, and scheduling test drives. </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row pad9x align-items-center"  style={{ paddingBottom: "50px" }}>
+            <div className="col-lg-5 align-self-center">
+              <div className="commen-rit-info">
+                <h3>Lead Disposition Form </h3>
+                <p>When you connect the system with <Link href="/products/microsoft-dynamics-365-marketing/"><a>Dynamics 365 Marketing</a></Link>, it will start showcasing the inbound lead data. Check out the below image that fetches the data based on customer activity. Based on the numbers, it will show you the score of Predictive Lead Scoring and a log of customer activity. </p>
+             
+              </div>
+            </div>
+            <div className="col-lg-7 align-self-center">
+              <div className="commen-lef-pic">
+              <img src="/img/n-3.png" alt="pic" />
+              </div>
+            </div>
+          </div>
+          <div className="row pad9x align-items-center">
+          <div className="col-lg-7 align-self-center">
+              <div className="commen-lef-pic">
+              <img src="/img/n-4.png" alt="pic" />
+              </div>
+            </div>
+            <div className="col-lg-5 align-self-center">
+              <div className="commen-rit-info">
+                <h3>Vehicle Inventory Dashboard </h3>
+                <p>You can easily embed the <Link href="/products/microsoft-power-bi/"><a>Power BI dashboard</a></Link> in Dynamics 365 Automotive Accelerator to get real-time insights. This helps the representative get the data on the total remaining inventory, available stocks, list price by model, and further details.</p>
+             
+              </div>
+            </div>
+           
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10">
+              <header className="commen-rit-info">
+                <p>Dynamics 365 Automotive Accelerator is a robust and effective solution when it comes to streamlining the automotive operations of the business. Capable of integrating with native as well as third-party solutions, it molds itself based on customer as well as business requirements. </p>
+
+<p>Implementing Dynamics 365 Automotive Accelerator will supercharge business growth while also powering the business to achieve business goals and resolve complex challenges. </p>
+
+<p>Dynamics Square is committed to helping growing businesses by all possible means. For the last 12+ years, we have been offering our <Link href="/our-services/dynamics-365-support-services/"><a>Dynamics 365 support</a></Link>, <Link href="/our-services/dynamics-365-implementation-services/"><a>implementation</a></Link>, and <Link href="/our-services/dynamics-365-upgrade-services/"><a>upgrade services</a></Link> to global customers. </p>
+<p>With 150+ consultants and 500+ customers, Dynamics Square has become a reliable Microsoft Solutions Partner. In case of need, you can write us an email at info@dynamicssquare.co.uk or call us at +44-207-193-2502 directly to get instant help. </p>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Finance;
