d1600d1ddbc81210749e899f8aa352080ee777c2
Sushil Behera <sbehera@mpgbis.com>
2023-08-07 20:20:21 +0530

    update new bespoke page

---
+++ pages/bespoke-crm-system.js
@@ -0,0 +1,349 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const Startup = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Contact a Microsoft-Certified CRM Consultant NOW! </title>
+        <meta
+          name="description"
+          content="Implement a bespoke CRM system solution to boost customer experience, achieve maximum customer satisfaction, and generate more sales. Implement Dynamics 365 CRM today! "
+        />
+        <link
+          rel="canonical"
+          href="https://www.dynamicssquare.co.uk/bespoke-crm-system/"
+        />
+      </Head>
+      <main>
+        <section className="Solution-banner hero-1 hero">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 mobile-1 align-self-center">
+                <h1>Bespoke <br /> CRM System</h1>
+                <p>Supercharge Customer Relationship Management and enhance the customer experience by implementing bespoke CRM systems for your Unique Business. </p>
+                <div className="text-center text-lg-start">
+                  <Link href="#exampleModal">
+                    <a
+                      data-bs-toggle="modal"
+                      className="btn-get-started scrollto"
+                    >
+                      <span>Let's Get Started!</span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-lg-6 align-self-center">
+                <Image
+                  src="/img/CRM-header-banner.png"
+                  alt="Bespoke CRM System"
+                  width={805} height={473}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <header className="section-header">
+              <h2>Power Up your Business with Bespoke CRM Solutions </h2>
+              <p>Manage your sales, deliver an exceptional customer experience, track sales data, and get real-time insights to empower your business for growth and efficiency with a perfect bespoke CRM solution system. If you are a growing business, it is a must for you to implement a purpose-driven CRM solution to enhance your overall performance while maintaining customers at their best.  </p>
+              <p>Building a personal CRM solution for a business is a hectic, costly, and inefficient task as it requires a lot of resources to invest other than a huge amount of money. But customising a standard solution to make it perfect for a unique business is the best option you can opt for without any financial burden. Isn't it good and cost-effective? </p>
+              <p>A custom-built CRM solution will empower you to deal with the specific needs of a business while making it capable of managing, tracking, and monitoring all customer requirements. A bespoke CRM development acts like an out-of-the-box move to sustain tough market conditions and cater to unique customer queries.</p>
+            </header>
+          </div>
+        </section>
+        <section className="Solution-business-why custom-pd-1">
+          <div className="container">
+            <div className="row gx-5">
+              <div className="col-lg-6 align-self-center">
+                <h2>Why Choose <br /> Microsoft Dynamics <br /> 365 CRM? </h2>
+                <p><Link href="/products/microsoft-dynamics-crm/"><a>Microsoft CRM</a></Link> offers robust features and flexibility for all growing businesses while providing extraordinary capabilities to meet all customer management needs. Being a powerful CRM solution, it can be personalised to match the CRM requirements of a business.</p>
+              </div>
+              <div className="col-lg-6 align-self-center">
+                <div className="content-icons-modil-1">
+                  <div className="content-inner-moil-1">
+                    <div className="icons-left">
+                      <div className="ext-bord">
+                        <i className="bi bi-check2"></i>
+                      </div>
+                    </div>
+                    <div className="cintent-right">
+                      <p>D365 CRM smooths the sales cycle, resulting in closing more deals, acquiring more prospects, building trust, and achieving sales targets. </p>
+                    </div>
+                  </div>
+
+                  <div className="content-inner-moil-1">
+                    <div className="icons-left">
+                      <div className="ext-bord">
+                        <i className="bi bi-check2"></i>
+                      </div>
+                    </div>
+                    <div className="cintent-right">
+                      <p>Streamline marketing campaigns, avail built-in analytics, track customer data for personalised offers, and retain customers for the long run. </p>
+                    </div>
+                  </div>
+
+                  <div className="content-inner-moil-1">
+                    <div className="icons-left">
+                      <div className="ext-bord">
+                        <i className="bi bi-check2"></i>
+                      </div>
+                    </div>
+                    <div className="cintent-right">
+                      <p>Offer an enhanced customer experience, access unified data on all customers, enhance customer segmentation, and establish an ecosystem of centralised communication. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="call_to_action call_to_action-height">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5">
+                <div className="call_to_action-right over-img">
+                  <img src="/img/MaskGroup8.png" alt="imagea" />
+                </div>
+              </div>
+              <div className="col-lg-7 align-self-center">
+                <div className="call_to_action-left">
+                  <h3>Get a purpose-driven CRM Solution for your Unique business. </h3>
+                  <p>Implement the Microsoft Dynamics 365 CRM solution to raise above-standard CRM functionalities. </p>
+                  <div className="m-o-t m-o-t-inde">
+                    <Link href="#exampleModal">
+                      <a
+                        data-bs-toggle="modal"
+                        className="btn-get-started scrollto"
+                      >
+                        <span>Schedule a FREE Demo!</span>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="new-style-bg">
+        <div className="container">
+          <div className='row justify-content-center'>
+            <div className='col-lg-9'>
+            <header className="section-header">
+            <h2>Benefits of having <br /> a Tailor-Made Microsoft CRM Solution </h2>
+          </header>
+            </div>
+          </div>
+          <div className="row g-5 pad2xyr">
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">1</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Low Cost of Ownership </h3>
+                  <p>Choosing Microsoft CRM over others enables you to save on investment, maintenance, and running costs with the trust of Microsoft. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">2</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Subscription-Based Model </h3>
+                  <p>Being a cloud-based system, you don't need to pay hefty amounts of money to build a solution that meets all demands. You can easily customise the <Link href="/products/microsoft-dynamics-crm/"><a>D365 CRM</a></Link> to make it perfect while paying through the monthly subscription model.</p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">3</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Align with Unique Business Needs </h3>
+                  <p>Irrespective of the complexities a business contains, you can personalise the system to deal with your changing business demands. Maybe you focus more on sales than service. It lets you enhance the capabilities of your sales processes while maintaining customer service. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">4</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Implement and eliminate the Desired Tool or Feature </h3>
+                  <p>The best part of bespoke CRM solutions is having the liability to implement and eliminate necessary or redundant features from the system, making it more focused and productive. </p>
+                </div>
+              </div>
+
+              
+            </div>
+            <div className="col-lg-6">
+            <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">5</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Auto Updates and Upgrades </h3>
+                  <p>Microsoft updates and upgrades all its systems, whether standard or customised, to meet modern business demands without hampering any of the existing tools, features, or processes—free of charge. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">6</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Advanced Reporting Tools </h3>
+                  <p>You can access advanced reporting tools (like <Link href="/products/microsoft-power-bi/"><a>Power BI</a></Link>) to understand customer behaviour towards a product or a service and make relatable changes. It empowers the business to supply what customers want, resulting in a better customer experience and retention. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">7</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Seamless Integrations </h3>
+                  <p>Integrate different applications or add-ons to enhance the capabilities while developing a bespoke CRM system. It allows the system to deal with complex areas of customer relationship management.</p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">8</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Enhanced Focus and Productivity </h3>
+                  <p>You can add or eliminate the elements on the CRM dashboard to enhance the focus, efficiency, and productivity of the sales team. It lets the team handle the customer and their requirements more efficiently.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+        <section className="call-to-action-center hero-4">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-9">
+                <div className="call-to-action-content">
+                  <h3>Still Confused? Talk to Us and Find out your Actual CRM Requirements </h3>
+                  <p>Microsoft-certified CRM experts are available to help you understand your CRM requirements so that you can build a value for money bespoke CRM solution.  </p>
+                  <div className="text-center m-o-t m-o-t-hv">
+                    <Link href="#exampleModal">
+                      <a
+                        data-bs-toggle="modal"
+                        className="btn-get-started scrollto"
+                      >
+                        <span>Let's Get Started </span>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="new-style-bg" style={{backgroundColor:'#fdfdfd'}}>
+        <div className="container">
+          <div className='row justify-content-center'>
+            <div className='col-lg-9'>
+            <header className="section-header">
+            <h2>Five Essential Steps to <br /> a Perfect Bespoke CRM Solution </h2>
+            <p>Before choosing an <Link href="/products/microsoft-dynamics-erp/"><a>ERP solution</a></Link>, you need to understand these essential steps for personalising a CRM solution.</p>
+          </header>
+            </div>
+          </div>
+          <div className="row g-5 pad2xyr justify-content-center">
+            <div className="col-lg-8">
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">1</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Shortlist the Basic Features </h3>
+                  <p>Based on your current business operations, you need to shortlist the basic features you need in a CRM system. It's like making a list of standard features and tools to run core customer relationship operations. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">2</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Understand Customer Needs </h3>
+                  <p>Collaborate with your team members and try to understand your customers demands. You need to put some effort into knowing current as well as future needs so that you can implement a future-ready CRM solution. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">3</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Shortlist the Advanced or Specific Capabilities </h3>
+                  <p>You need to make a list of capabilities that you need to deal with complex and unique business operations. It can be anything, from opportunity management to membership renewals. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">4</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Specify Budget </h3>
+                  <p>We know that there is no maintenance or running cost, but you need to specify a budget you want to spend to customise and implement the Microsoft solution as desired. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                <div class="wr-in"><span class="number-st">5</span></div>
+                </div>
+                <div className="style-grid-content">
+                  <h3>Choose a Partner </h3>
+                  <p>Now, find a Microsoft CRM <Link href="/our-services/dynamics-365-implementation-services/"><a>Dynamics 365 implementation partner</a></Link> that understands your business requirements and can furnish the bespoke CRM solution you need to grow and scale. Dynamics Square UK, can be a stepping stone to getting more sales, achieving more targets, and offering a great customer experience. </p>
+                </div>
+              </div>
+              
+            </div>
+           
+          </div>
+        </div>
+      </section>
+        <section className="busine_service_bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="busine_service_bottom-left">
+                  <div className="busine_service_bottom-right">
+                    <Image
+                      src="/img/startup-side.png"
+                      className="fix-im"
+                      alt="bisin_leftimg"
+                      width={546} height={524}
+                    />
+                   
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 d-flex align-items-center">
+                <div className="side-heading">
+                  <h2>Are you Ready to Achieve your Planned Sales Targets? </h2>
+                  <p>Implement Microsoft Dynamics 365 CRM to automate your sales, make your staff productive, and retain customers to get maximum output. </p>
+                  <div className="text-lg-start mm-t">
+                    <Link href="#exampleModal">
+                      <a
+                        data-bs-toggle="modal"
+                        className="btn-get-started scrollto"
+                      >
+                        <span>Contact a Microsoft-Certified CRM Consultant NOW! </span>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+   
+      </main>
+    </>
+  );
+};
+
+export default Startup;
