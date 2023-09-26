bb4184e582b3d7fc5f3f36c31d7605203c1e2eec
Sushil Behera <sbehera@mpgbis.com>
2023-09-26 17:14:42 +0530

    update new page

---
+++ pages/products/dynamics-365-business-central/crm-module.js
@@ -0,0 +1,216 @@
+import Head from "next/head";
+import Link from "next/link"; 
+import Image from 'next/image';
+import Form from "../../../components/Form";
+const CrmModule = () => {
+  return (
+    <>
+      <Head>
+        <title>CRM Module for Dynamics 365 Business Central | Dynamics Square UK</title>
+        <meta
+          name="description"
+          content="Manage customer and offer best customer experience utilizing the advanced capabilities of Dynamics 365 Business Central CRM Module."
+        />
+         <link
+          rel="canonical"
+          href="https://www.dynamicssquare.co.uk/products/dynamics-365-business-central/crm-module/"
+        />
+      </Head>
+      <section className="Solution-banner hero-1 hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mobile-4 align-self-center">
+              <h1>Dynamics 365 Business Central for Customer Relationship Management  </h1>
+              <p>Streamline tasks, strengthen customer relationships, offer utmost satisfaction, track all customers, and centralise data with the Dynamics 365 Business Central CRM Module</p>
+              <div>
+                <div className="text-center text-lg-start">
+                  <Link href="#exampleModal">
+                    <a
+                    data-bs-toggle="modal"
+                    
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Schedule a Demo Now! </span></a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <Image
+                src="/img/crm-module.png"
+                alt="CRM Module for Dynamics 365 Business Central"
+                width={709}
+                height={514}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="new-bgs">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-10">
+            <header className="section-header">
+            <h2>Dynamics 365 Business Central CRM Module: Effective CRM for SMBs </h2>
+            <p>An effective CRM solution is a must for all businesses to manage their customers' needs, understand their requirements, and offer the utmost level of satisfaction. The core CRM functionality empowers the sales team to cater to more clients while enhancing the overall customer experience. With a business-centric <Link href="/products/microsoft-dynamics-crm/"><a>CRM solution</a></Link>, you can accelerate sales while also accessing automation for increased1q efficiency. </p>
+            <p>Business Central is an absolute business-oriented ERP solution with powerful capabilities in customer relationship management. It offers different modules that include CRM, finance, supply chain, and inventory based on the licence you purchase. Irrespective of the Business Central licencing plan you choose Essentials or Premium, CRM module will come as standard.</p>
+            <p>Regardless of the complexity of your business, you can personalise <Link href="/products/dynamics-365-business-central/"><a>Microsoft Dynamics 365 Business Central</a></Link>  to align it with your unique business requirements. Also, you can integrate different add-ons and applications to enhance the overall efficiency of the Business Central CRM module.</p>
+          </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom001 custom0099">
+        <div className="container">
+          <header className="section-header">
+            <h2>What Is Microsoft Dynamics 365 Business Central?</h2>
+          </header>
+          <div className="row p-433">
+            <div className="col-lg-6 align-self-center">
+              <div className="custom002">
+                <p>Business Central is an all-in-one ERP solution capable of connecting and streamlining different departments of a business that include finance, sales, inventory, warehouse, customer service, and marketing. Being a cloud-based solution, it offers powerful capabilities to deal with modern business needs. </p>
+                <p>With a unified database, it unifies the information and simplifies business operations for enhanced output. Embedded with modern business technologies like AI, BI, and ML, Business Central comes with automation to enhance overall business efficiency and productivity. After <Link href="/dynamics-365-business-central-implementation/"><a>Business Central implementation</a></Link>, businesses have reported a 23% reduction in operational costs, an 80% reduction in IT costs, a 2X increase in efficiency, and a 30% increase in productivity. </p>
+                <p>Being a part of the <Link href="/products/microsoft-dynamics-365/"><a>Dynamics 365 group of applications</a></Link>, Business Central offers a number of benefits to businesses, including real-time decision-making, actionable data insights, a simple user interface, easy customization, and global connectivity to manage all locations.</p>
+              </div>
+            </div>
+            <div className="col-lg-1 align-self-center"></div>
+            <div className="col-lg-5 align-self-center">
+              <div className="busine_service_bottom-left common-side-form">
+                <Form />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom001">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+            <header className="section-header">
+            <h2>Generate More Revenue with Business Central CRM Module </h2>
+          </header>
+            </div>
+          </div>
+          <div className="row p-433">
+            <div className="col-lg-5 align-self-center">
+              <div className="left-pic-nn">
+                <Image
+                  src="/img/Dynamics-365-for-Health-care-side.png"
+                  alt="Dynamics-365-for-Health-care-side"
+                  width={422} height={453}
+                />
+              </div>
+            </div>
+
+            <div className="col-lg-1 align-self-center"></div>
+            <div className="col-lg-6 align-self-center">
+              <div className="custom002">
+                <p>The simplest way to generate more revenue is to manage customers to keep them engaged with your businesses through all possible means. Even business owners understand the value of managing, monitoring, and tracking customers to offer them the best possible experience. With an AI-enabled and powerful CRM solution, managing customers as well as their data can be effortless and hassle-free. </p>
+                <p>The Business Central CRM module is one such solution on the market that is trusted by businesses for its capabilities, effectiveness, and reliability. It seamlessly simplifies data management, team collaboration, and interaction with all prospects to generate more output.</p>
+                <p>You can find more potential opportunities and build up-sell & cross-sell opportunities to give what the customer wants at the right moment. With perfect CRM functionality, you can easily track customer data to create personalised offers and discounts for more revenue with limited resources. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="call-to-action-center hero-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <div className="call-to-action-content">
+                <h3>Dynamics 365 Business Central for Customer Relationship Management </h3>
+                <p>Understand your customers, utilise data, and avail sales automation with the CRM module of Business Central.</p>
+                <div className="text-center m-o-t new-btn-nn">
+                  <Link href="#exampleModal">
+                    <a
+                    data-bs-toggle="modal"
+                    
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Book a Free Demo Today! </span></a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="new-style-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+            <header className="section-header">
+            <h2>Dynamics 365 Business Central CRM: Top Features You Should Checkout</h2>
+            <p>The CRM module of Dynamics 365 Business Central offers sufficient capabilities required for a SMB. These features empower a business to streamline different operations that fall under core CRM functionality. Along with the customer details, Business Central empowers businesses to boost efficiency, enhance customer satisfaction, support their sales teams, and offer real-time data insights to make great decisions. </p>
+          </header>
+            </div>
+          </div>
+          <div className="row g-5 pad2xyr">
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Monitor Contacts </h3>
+                  <p>You can easily monitor all the activity of each contact, along with their data, with a few clicks. These contacts contain information about your customers, along with the ability to edit the data anytime and anywhere. You can get information like role, designation, address, job experience, previous business, and much more. </p>
+                </div>
+              </div>
+
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Track Salespeople Activity </h3>
+                  <p>A business has different salespeople who manage various customers. With the Business Central CRM module, you can easily link an account with specific salespeople so that personalised offers can be made based on customer interaction and data. Inside the Sales People option, you will be able to access more tools, like tasks, opportunities, and more.</p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Create Tasks </h3>
+                  <p>Based on customer interaction and the planned sales strategy, you can easily create different tasks and assign them to the sales team. Also, it enables the team to make personalised notes to access the required data whenever desired. </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Opportunity Tracking </h3>
+                  <p>Track all the possible sales opportunities to make each sale count while generating more and more revenue with limited resources. It lets you manage all potential sales opportunities from the unified Sales People platform. </p>
+                </div>
+              </div>
+
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Build Different Marketing Campaigns </h3>
+                  <p>A campaign in the CRM module will let you target different customers with personalised offers. You can make a list of different customers, known as segments, to approach them all with a similar campaign. It can help you convert more prospects or leads into customers with personalised targeting. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Generate Sales Quota </h3>
+                  <p>A Sales Quota is the invoice you send to the customer after he or she agrees to your service. You can easily build a sales quote using predefined templates in the Business Central CRM module and send it directly to the customer. If there is a requirement, you can edit it later without any hassle.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-shape custom0099">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-10">
+              <div className="commen-rit-info text-center">
+                <h3>Embrace Business Central CRM Functionality with Dynamics Square </h3>
+                <p>Business Central is an AI-driven ERP and CRM solution that can revolutionise the way a business deals with internal as well as external processes. It will transform and accelerate your business towards growth and scalability. </p>
+                <p>The Business Central CRM module not only simplifies customer management but also boosts sales. It can help you track sales performance, track potential opportunities, utilise data in real-time, and enhance overall revenue.</p> 
+                <p>Regardless of your industry, you can implement Business Central along with its CRM module to offer personalised deals, increase customer visibility, prioritise potential leads, and enhance overall sales efficiency. </p>
+                <p>For more details, like <Link href="/dynamics-365-business-central-pricing-licensing/"><a>Business Central pricing and licencing plans</a></Link>, feel free to contact Dynamics Square, the trusted and reliable Microsoft Solutions Partner in the UK.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CrmModule;
