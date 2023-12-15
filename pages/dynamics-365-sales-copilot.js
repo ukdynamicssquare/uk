fdbaca42b31a720b8a1c0669c6a21df4807948f5
Sushil Behera <sbehera@mpgbis.com>
2023-12-15 20:33:15 +0530

    update new page

---
+++ pages/dynamics-365-sales-copilot.js
@@ -0,0 +1,279 @@
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import Link from "next/link";
+
+
+const Dynamics365SalesCopilot = () => {
+  return (
+    <>
+      <Head>
+      <title>Dynamics 365 Sales Copilot | Dynamics Square</title>
+        <link rel="canonical" href="https://www.dynamicssquare.co.uk/dynamics-365-sales-copilot/" />
+        <meta name="description"
+          content="Strengthen customer relationships, avail AI-powered sales insights, and automate sales processes leveraging the power of Dynamics 365 Sales Copilot."
+        />
+
+      </Head>
+      <section className="Solution-banner hero-1 hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mobile-1 align-self-center">
+              <h1>
+              Dynamics 365 Sales Copilot 
+              </h1>
+              <p>Simplify complex customer relationships, get AI-powered sales insights, and automate the sales process with Dynamics 365 Sales Copilot. </p>
+              <div>
+                <div className="text-center text-lg-start">
+                  <Link href="#exampleModal"><a
+                    data-bs-toggle="modal"
+                  
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Schedule a Demo Now! </span></a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <Image src="/img/copilate-banner.png" alt="copilate-banner" width={728} height={447} />
+            </div>
+          </div>
+        </div>
+      </section>
+  
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-10">
+            <header className="section-header">
+            <h2>What Is Dynamics 365 Sales Copilot? </h2>
+            <p>Copilot is an enterprise-ready AI that harnesses the power of the latest technology by combining the LLM (large language models) with your data available in <Link href="/products/microsoft-dynamics-365-sales/"><a>Microsoft Dynamics 365 Sales</a></Link>. Copilot is backed by OpenAI's ChatGPT technology, leveraging sophisticated data processing and orchestration engine technology to unlock business productivity and overall effectiveness. Businesses can utilize Copilot to avail themselves of a quick summary of a call or an email, track lead records, monitor sales updates, collect data from meetings, and get the latest update about a client. </p>
+            <p>Being a smart AI assistant, Copilot comes with a chat interface that can be integrated into any Microsoft application. Using normal languages can help your team members directly capture the data in real-time from Outlook, Teams, or any other app in the Microsoft 365 ecosystem. </p>
+          </header>
+            </div>
+          </div>
+         
+
+
+          <div className="row g-5">
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image src="/img/Enhance-your-Business-Productivity.png" alt="Enhance-your-Business-Productivity" width={94} height={94} />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Enhance your Business Productivity</h3>
+                  <p>Automate your sales processes and simplify various communication tasks with embedded AI. Get summaries of long meetings, generate sales call transpiration, and revert emails with an interactive reply with just a click. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image src="/img/Offer-Bespoke-Customer-Interaction.png" alt="Offer-Bespoke-Customer-Interaction"  width={94} height={94} />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Offer Bespoke Customer Interaction </h3>
+                  <p>Get real-time call or meeting insights and recommend relevant resources to clients to build strong relationships with customers. Also, leverage AI to analyse the call quality post-communication to prepare for the next call. </p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image src="/img/Build-Sustainable-Workflow.png" alt="Build-Sustainable-Workflow"  width={94} height={94} />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Build Sustainable Workflow </h3>
+                  <p>Connect different Microsoft apps to build a sustainable connection for efficient data workflow between <Link href="/products/microsoft-dynamics-crm/"><a>Microsoft CRM</a></Link> and other apps. Create, update, or edit data in one application and find the changes in another. </p>
+                </div>
+              </div>
+            
+            </div>
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image
+                    src="/img/Command-using-Simple-Language.png"
+                    alt="Command-using-Simple-Language"
+                    width={94} height={94}
+                  />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Command using Simple Language </h3>
+                  <p>Use your simple language to communicate with AI and get product results with just a click. You can give a command like, “Give me the summary of a meeting held with Mr. Nitesh from Dynamics Square”, and your copilot will generate the summary based on your communication. </p>
+                </div>
+              </div>
+
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image src="/img/Analyse-Conversation.png" alt="Analyse-Conversation"  width={94} height={94} />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Analyse Conversation </h3>
+                  <p>Copilot comes with various features that help you analyse a conversation with the client based on sentiments and pre-determined KPIs. These KPIs include talking or playback speed and the talk-to-listen ratio. </p>
+                </div>
+              </div>
+
+              <div className="style-grid-1">
+                <div className="style-grid-icons">
+                  <Image
+                    src="/img/Automates-core-Processes.png"
+                    alt="Automates-core-Processes"
+                    width={94} height={94}
+                  />
+                </div>
+                <div className="style-grid-content">
+                  <h3>Automates core Processes </h3>
+                  <p>There are certain processes in sales that restrict the team from achieving good performance. Copilot lets you automate such recurring and manual operations to empower businesses to focus on growth and converting more sales. </p>
+                </div>
+              </div>
+             
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom05">
+        <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-lg-10">
+            <header className="section-header">
+            <h2>Dynamics 365 Sales Copilot Capabilities</h2>
+            <p>Explore the core capabilities of Dynamics Sales Copilot that you can utilise to smooth your sales efforts. </p>
+          </header>
+            </div>
+          </div>
+         
+          <div className="row">
+            <div className="col-lg-6 align-self-center">
+              <div className="busine_service_bottom-left custom012">
+                <Image
+                  src="/img/copilate-side-pic.png"
+                  alt="microosoft-partner.png"
+                  width={840} height={566}
+                />
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <div className="side-text-new">
+                <div><h4>Generate Quick Summaries </h4></div>
+                <p>Track all your opportunities and lead records to generate a quick summary for an efficient sales pitch. In general, Dynamics 365 Sales builds a summary from default data, but you can add or remove fields based on relevancy to get personalised opportunity data. </p>
+              </div>
+              <div className="side-text-new">
+                <div><h4>Collect Data from a Meeting </h4></div>
+                <p>Copilot is capable enough to combine and collect data from leads, opportunities, and previous communication to help you prepare for the next meeting. </p>
+              </div>
+              <div className="side-text-new">
+               <div> <h4>Email Automation </h4></div>
+                <p>Access the Copilot to track all emails, revert with a professional email, summarise long emails in a few lines, and get notified to send follow-up emails to different clients. </p>
+              </div>
+              <div className="side-text-new">
+                <div><h4>Get Client Status in Real-time </h4></div>
+                <p>Get informed about any of the changes your client has made. You can simply ask your copilot about the latest updates, and it will give you a list of the latest changes made to a particular client’s account. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="call-to-action-center hero-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="call-to-action-content">
+                <h3>Automate Sales Processes and Maximise Productivity </h3>
+                <p>Drive new business opportunities, connect internal teams, and offer a best-in-class customer experience by implementing Dynamics 365 Sales Copilot. </p>
+                <div className="text-center m-o-t new-btn-nn">
+                  <Link href="#exampleModal"><a
+                    data-bs-toggle="modal"
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Get Copilot Now! </span></a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section id="pricing" className="pricing">
+        <div className="container">
+          <header className="section-header">
+            <h2>Dynamics 365 Sales Copilot Pricing </h2>
+            <p>Sales Copilot comes standard with Dynamics 365 Sales Enterprise and Premium plans. To access Copilot in other sales plans, you need to pay a monthly subscription fee based on the user.</p>
+          </header>
+          <div className="row gy-4 p-9">
+            <div className="col-lg-3">
+              {/* <div className="box">
+                        <h4>Business Central<br />
+                           Essentials
+                        </h4>
+                        <div className="price"><sup>£</sup>52.80<span>Per-User/Per Month</span></div>
+                        <ul>
+                           <li> <i className="bi bi-check-lg"></i> Financial Management</li>
+                           <li> <i className="bi bi-check-lg"></i> Inventory Management</li>
+                           <li> <i className="bi bi-check-lg"></i> Order Management</li>
+                           <li> <i className="bi bi-check-lg"></i> Purchase Order Management</li>
+                           <li> <i className="bi bi-check-lg"></i> Project Management</li>
+                           <li> <i className="bi bi-check-lg"></i> Additional Capabilities</li>
+                        </ul>
+                       
+                     </div> */}
+            </div>
+            <div className="col-lg-6">
+              <div className="box box-22">
+                <h3>Dynamics 365 Sales Copilot </h3>
+                <div className="price">
+                £33.00<span>Per-User/Month</span>
+                </div>
+                <ul>
+                  <li className="text-center">
+                  Access complete Copilot Capabilities 
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              {/* <div className="box">
+                        <h3>Business Central<br />
+                           Essentials
+                        </h3>
+                        <div className="price"><sup>£</sup>52.80<span>Per-User/Per Month</span></div>
+                        <ul>
+                           <li> <i className="bi bi-check-lg"></i> Read and Approve</li>
+                           <li> <i className="bi bi-check-lg"></i> Employee Self Serve</li>
+                           <li> <i className="bi bi-check-lg"></i> Run All Reports</li>
+                        </ul>
+                       
+                     </div> */}
+            </div>
+
+            <div className="col-lg-12 col-md-12 pric-t-p">
+              <p>To access Sales Copilot in Outlook, Excel, and Microsoft Teams, you will need a product licence for <Link href="/products/microsoft-dynamics-365/"><a>Dynamics 365</a></Link> or Office 365 for Enterprise. For a detailed Dynamics 365 sales pricing structure, you can check out the official Microsoft Dynamics 365 page.</p>
+              <div className="price-btn price-btnasd d-flex justify-content-center">
+                <Link  href="#exampleModal">
+                  <a
+                  data-bs-toggle="modal"
+                 
+                  className="btn-get-red"
+                >
+                  <span>Get Started Now</span></a>
+                </Link>
+                {/* <Link href="#about" className="btn-get-whit">
+                              <span>See Licensing Guide</span>
+                              </Link> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  );
+};
+
+export default Dynamics365SalesCopilot;
+
